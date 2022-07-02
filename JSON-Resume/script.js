5c88a2531e2985a2ff0662e72475376f7fd53ed9
assaypoulose <94008297+assaypoulose@users.noreply.github.com>
2022-07-02 07:35:56 +0300

    Add files via upload

---
+++ JSON-Resume/script.js
@@ -0,0 +1,59 @@
+let myResume={
+    "basics": {
+      "name": "Assay Sooraj",
+      "email": "assaypoulose16@gamil.com",
+      "phone": +37126471323,
+      "degree": "B.E.Aeronautical Engineering",
+      "location": {
+        "address": "Hipokrata iela 13-52",
+        "postalCode": 1079,
+        "city": "Riga",
+        "state": "Riga",
+        "country": "Latvia"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/assay-poulose-961911179/",
+          "github":"https://github.com/assaypoulose"
+        }
+      ]
+    },
+    "work": [
+        {
+          "company": "Innovative Travel Solutions",
+          "position": "GDS Expert",
+          "startDate": "2022-06-06",
+          "endDate": "Currently working",
+          "summary": "I am a GDS expert in Apollo software, where flight connections are built and still gaining lots of experience",
+        },
+      ],
+      "education": [
+        {
+          "institution": "Riga Technical University",
+          "department": "Aviation Transport",
+          "studyType": "fulltime",
+          "batch start year": 2018,
+          "batch end year": 2020,
+          "gpa": 6.5,
+        }
+      ],
+      "skills": [
+        {
+          "name": "python,javascript",
+          "level": "beginer",
+        }
+          
+    ],
+        
+      "languages": [
+        {
+          "language": "Enlish,Hindi,Tamil,Malayalam,Latvian",
+        }
+      ],
+      "interests": [
+        {
+          "name": "script writter,Painting,Dancing,Reading",
+        }
+      ]
+    }
+    console.log(myResume);
